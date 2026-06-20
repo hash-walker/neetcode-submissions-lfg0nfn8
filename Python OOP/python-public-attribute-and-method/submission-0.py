3b23510691e5c2a9178c80795a8e59b206c725da
Hamza Faraz <124732544+hash-walker@users.noreply.github.com>
2026-06-21 01:34:30 +0500

    Add: python-public-attribute-and-method - submission-0

---
+++ Python OOP/python-public-attribute-and-method/submission-0.py
@@ -0,0 +1,13 @@
+class StoreItem:
+    def __init__(self, item, price):
+        self.item = item 
+        self.price = price
+
+
+chips = StoreItem("Chips", 1.99) # Don't modify this line
+
+# TODO: Access the attributes of the chips object and display them
+
+print(f"Item: [{chips.item}] - Price$[{chips.price}]")
+
+
